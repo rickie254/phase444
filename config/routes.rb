d1f1f77ec3c49c909b213c28bc7162af47b5ac5f
Bryan Bosire <bryanbosire@gmail.com>
2022-10-11 23:27:09 +0300

    created users resources in routes CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   
+  resources :users
+
+  # POST /signup
+  post '/signup', to: 'users#create'
+  # GET /me
+  get '/me', to: 'users#show'
+
+  #  POST /login
+  post '/login', to: 'sessions#create'
+  #  DELETE /logout
+  delete '/logout', to: 'sessions#destroy'
+
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
